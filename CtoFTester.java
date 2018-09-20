7b5b6ee455621637e965dc199fc9379587dba7b4
CodyBagwandeen <cbagwandeen@stuy.edu>
2018-09-19 20:44:26 -0400

    added the java files

---
+++ CtoFTester.java
@@ -0,0 +1,13 @@
+public class CtoFTester {
+	public static void main(String[] args){
+}
+		public static int cToF(int temp) {
+			return ( ( temp * 9 / 5) + 32);
+		}	
+		public static int fToC(int temp) {
+			return ( ( temp - 32) * 5  /9 );
+		}
+}
+
+
+	
